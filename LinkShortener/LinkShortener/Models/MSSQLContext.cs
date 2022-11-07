9007ecfa3594e1dca5302e453fda052345bbfb2e
ToptyzhkinMult <topyzhkin@gmail.com>
2022-11-07 16:16:31 +0300

    Добавлен контекст и строка подключения

---
+++ LinkShortener/LinkShortener/Models/MSSQLContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkShortener.Models
+{
+    public class MSSQLContext : DbContext
+    {
+        public MSSQLContext(DbContextOptions<MSSQLContext> options) : base(options)
+        {
+
+        }
+    }
+}
