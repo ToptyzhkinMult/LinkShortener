29f7479e22f84f38c971d2981411f5f190cebfa2
ToptyzhkinMult <topyzhkin@gmail.com>
2022-11-07 18:45:15 +0300

    Добавлена заглушка для создания коротких ссылок

---
+++ LinkShortener/LinkShortener/Helper/ShortLinkGenerator.cs
@@ -0,0 +1,25 @@
+﻿using LinkShortener.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkShortener.Helper
+{
+    public class ShortLinkGenerator
+    {
+        private readonly MSSQLContext _context;
+
+        public ShortLinkGenerator(MSSQLContext context)
+        {
+            _context = context;
+        }
+        public string GenerteShortLink()
+        {
+            string shortLinlk = "https://www.google.com/" + (new Random()).Next(1000, 9999);
+            if (_context.Link.Any(l => l.ShortLink == shortLinlk)) shortLinlk = GenerteShortLink();
+
+            return shortLinlk;
+        }
+    }
+}
