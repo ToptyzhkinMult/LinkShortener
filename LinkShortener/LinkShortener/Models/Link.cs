edbbd2df986c3982867e671b4ed9171c3f416a5b
ToptyzhkinMult <topyzhkin@gmail.com>
2022-11-07 16:37:46 +0300

    Добавлена модель Link

---
+++ LinkShortener/LinkShortener/Models/Link.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinkShortener.Models
+{
+    public class Link
+    {
+        public int Id { get; set; }
+
+        public string OriginalLink { get; set; }
+
+        public string ShortLink { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public int FolowingCount { get; set; }
+    }
+}
